36b66a594b59e6a6f8a26787add604a83028216d
Obrusnik Vit <vit.obrusnik@gmail.com>
2018-08-28 22:24:50 +0200

    Version 2 OK. Version 2_quant better now without sampling in regulating function. Function to disp parameters for better orientation.

--- init_params.m
+++ init_params.m
@@ -23,20 +23,18 @@ T3= N* MFC3(3)*sin(MFC3(2)*atan(MFC3(1)*s-MFC3(4)*(MFC3(1)*s-atan(MFC3(1)*s))));
 %% Slip regulator tuning parameters
 % der_TH = [50 20 8 4 2 1 0.8 0.6 0.2 0 -1];
 % dM_val = [128 128 128 64 32 16 -2 -4 -8 -16 -32 0];
-% der_TH = [5 0.05 -0.01 -1];
-% dM_val = [100 10 0 -10]; % works for ice
 
-% der_TH = [20 4 2 1 0.8 0.6 0.2 0];
-% dM_val = [16 8 4 2 -1 -2 -4 -8 -10]; %this works so so..
+% der_TH = [1000 500 200 40 20 10 5 2 1 -1 -2 ];
+% dM_val = [128 64 32 16 8 4 0.1 -0.1 -1 -2 -4 -8]; %Ts 0.005 dry quite OK, trans bad
 
-der_TH = [100 50 20 4 2 1 0.8 0.6 0.2 0];
-dM_val = [24 16 8 4 2 1 -0.5 -1 -2 -4 -8]; %
+der_TH = [1000 500 200 40 20 10 5 2 1 -1 -2 ];
+dM_val = [128 64 32 16 8 4 1 0.1 -1 -2 -4 -8]; %
 
 assert(numel(der_TH)+1 == numel(dM_val), "Thershold and values len");
 
 CRITICAL_SLIP_VEL = 10;
 ERR_TOLERABLE = 0.1;            %tolerable error in wanted M and resulting M
-Ts = 0.001;                     %recomputing at 1000Hz
+Ts = 0.005;                     %recomputing at 1000Hz
 
 par_ctr = struct;
 par_ctr.der_TH = der_TH;
---
+++ show_TH_val.m
@@ -0,0 +1,8 @@
+function  show_TH_val(der_TH, dM_val)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+for i = 1:numel(der_TH)
+    disp(der_TH(i) + ", " + dM_val(i))
+end
+end
+
